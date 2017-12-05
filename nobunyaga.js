2ef71a97947a2b59af6f67f7f17593189147b8f5
Ucandoit <yukan.zhao@gmail.com>
2017-12-06 00:04:33 +0100

    ajustement to the new update + new feature training

--- nobunyaga.js
+++ nobunyaga.js
@@ -1,33 +1,51 @@
 // console.log(location.href);
 if (location.href.indexOf("app0.mixi") >= 0 || location.href.indexOf("app.mbga") >= 0 || location.href.indexOf("ld.mygc") >= 0) {
 	var xmModel = {
-		questId: 1
+		questId: 1,
+		resources: {
+			fire: { buildingClass: 'type03', train: ['fire', 'sky', 'earth'] },
+			earth: { buildingClass: 'type04', train: ['earth', 'fire', 'wind'] },
+			wind: { buildingClass: 'type05', train: ['wind', 'earth', 'water'] },
+			water: { buildingClass: 'type06', train: ['water', 'wind', 'sky'] },
+			sky: { buildingClass: 'type07', train: ['sky', 'water', 'fire'] }
+		},
+		train: {
+			catPrefix: 'face-card-id',
+			level: {
+				0: { resources: [8,2,0], time: 10 },
+				1: { resources: [13,3,0], time: 15 },
+				2: { resources: [23,5,0], time: 20 },
+				3: { resources: [38,9,0], time: 25 },
+				4: { resources: [60,15,0], time: 50 },
+				5: { resources: [89,22,0], time: 80 },
+				6: { resources: [125,31,0], time: 120 },
+				7: { resources: [168,42,0], time: 180 },
+				8: { resources: [218,54,0], time: 300 },
+				9: { resources: [275,68,0], time: 480 },
+				10: { resources: [342,85,34], time: 720 },
+				11: { resources: [419,104,41], time: 1050 },
+				12: { resources: [506,126,50], time: 1470 },
+				13: { resources: [603,150,60], time:  1980},
+				14: { resources: [710,177,71], time: 2580 },
+				15: { resources: [827,206,82], time: 3270 },
+				16: { resources: [959,239,95], time: 4050 },
+				17: { resources: [1106,276,110], time: 4920 },
+				18: { resources: [1268,317,126], time: 5880 },
+				19: { resources: [1445,361,144], time: 6960 }
+			}
+		}
 	};
 
-	// 0 dazei
-	// 1 read report
-	// 2 draw
-	// 3 construct house
-	// 4 move cat
-
 	var xmActions = {
-		// clickAction: function(selector, resolve) {
-		// 	if (selector.length > 0) {
-		// 		selector[0].click();
-		// 		resolve();
-		// 	} else {
-		// 		setTimeout(function() {
-		// 			xmActions.clickAction(selector, resolve);
-		// 		}, 3000);
-		// 	}
-		// },
 		debug: function(_message) {
 			console.log(_message);
 		},
 		doQuest: function() {
 			switch (xmModel.questId) {
 				case 1:
-					xmActions.toMap(xmActions.fightEnemy);
+					xmActions.toMap(function() {
+						xmActions.fightEnemy(false);
+					});
 					break;
 				case 2:
 					xmActions.toReport(xmActions.readReport);
@@ -50,41 +68,14 @@ if (location.href.indexOf("app0.mixi") >= 0 || location.href.indexOf("app.mbga")
 					});
 					break;
 				case 8:
-					xmActions.toHome(function() {
-						xmActions.constructBuilding('area.map01');
-					});
+					xmActions.toHome(xmActions.trainCat);
 					break;
 				case 9:
-					xmActions.toHome(function() {
-						xmActions.constructBuilding('area.map02');
+					xmActions.toMap(function() {
+						xmActions.fightEnemy(true);
 					});
 					break;
 				case 10:
-					xmActions.toHome(function() {
-						xmActions.constructBuilding('area.map05');
-					});
-					break;
-				case 11:
-					xmActions.toHome(xmActions.trainCat);
-					break;
-				case 12:
-					xmActions.toHome(function() {
-						xmActions.constructBuilding('area.map06');
-					});
-					break;
-				case 13:
-					xmActions.toHome(xmActions.developSkill);
-					break;
-				case 14:
-					xmActions.toTeam(xmActions.addSkill);
-					break;
-				case 15:
-					xmActions.toTeam(xmActions.switchLeader);
-					break;
-				case 16:
-					xmActions.toMap(xmActions.fightEnemy);
-					break;
-				case 17:
 					xmActions.toReport(xmActions.watchFight);
 					break;
 				default:
@@ -93,7 +84,7 @@ if (location.href.indexOf("app0.mixi") >= 0 || location.href.indexOf("app.mbga")
 		clickAction: function(_selector, _nextAction) {
 			if ($(_selector).length > 0) {
 				$(_selector)[0].click();
-				setTimeout(_nextAction, 2000);
+				setTimeout(_nextAction, 3000);
 			} else {
 				setTimeout(function() {
 					console.log('wait for web page rendering.');
@@ -139,37 +130,57 @@ if (location.href.indexOf("app0.mixi") >= 0 || location.href.indexOf("app.mbga")
 				xmModel.questId = xmModel.questId + 1;
 				xmActions.doQuest();
 			});
-			// if ($('input[alt=報酬を受け取る]').length > 0) {
-			// 	$('input[alt=報酬を受け取る]').eq(0).click();
-			// 	setTimeout(function() {
-			// 		console.log('next');
-			// 	}, 1000);
-			// } else {
-			// 	setTimeout(xmActions.completeQuest, 3000);
-			// }
 		},
-		fightEnemy: function() {
+		fightEnemy: function(_withboss) {
 			xmActions.debug('xmActions.fightEnemy');
-			xmActions.clickAction('.map_point_e', xmActions.confirmFight);
+			xmActions.clickAction('.map_point_e', function() {
+				xmActions.confirmFight(_withboss);
+			});
 		},
-		confirmFight: function() {
+		fightBoss: function() {
+			xmActions.debug('xmActions.fightBoss');
+			xmActions.clickAction('.map_point_e[alt=敵軍]', function() {
+				xmActions.confirmFight(false);
+			});
+		},
+		confirmFight: function(_withboss) {
 			xmActions.debug('xmActions.confirmFight');
-			xmActions.clickAction('.quest-ok-button img:visible', xmActions.goFight);
+			xmActions.clickAction('.quest-ok-button img:visible', function() {
+				xmActions.goFight(_withboss);
+			});
 		},
-		goFight: function() {
+		goFight: function(_withboss) {
 			xmActions.debug('xmActions.confirmFight');
-			xmActions.clickAction('#dialog_g', xmActions.confirmGoFight);
+			xmActions.clickAction('#dialog_g', function() {
+				xmActions.confirmGoFight(_withboss);
+			});
 		},
-		confirmGoFight: function() {
+		confirmGoFight: function(_withboss) {
 			xmActions.debug('xmActions.confirmGoFight');
-			xmActions.clickAction('#btl-ok-button .neko-alert-button', xmActions.waitFight);
+			xmActions.clickAction('#btl-ok-button .neko-alert-button', function() {
+				xmActions.waitFight(_withboss);
+			});
 		},
-		waitFight: function() {
+		waitFight: function(_withboss) {
 			xmActions.debug('xmActions.waitFight');
 			if ($('#notify_count_main').length > 0) {
-				setTimeout(xmActions.waitFight, 5000);
+				setTimeout(function() {
+					xmActions.waitFight(_withboss);
+				}, 5000);
 			} else {
-				setTimeout(xmActions.toQuestMenu, 2000);
+				setTimeout(function() {
+					xmActions.confirmAlert(function() {
+						xmActions.confirmAlert(function() {
+							if (_withboss) {
+								setTimeout(function() {
+									xmActions.fightBoss();
+								}, 2000);
+							} else {
+								setTimeout(xmActions.toQuestMenu, 2000);
+							}
+						});
+					});
+				}, 1000);
 			}
 		},
 		readReport: function() {
@@ -197,19 +208,21 @@ if (location.href.indexOf("app0.mixi") >= 0 || location.href.indexOf("app.mbga")
 		},
 		waitFinish: function(_nextAction, confirmAlert) {
 			xmActions.debug('xmActions.waitFinish');
-			if ($.trim($("#doing").html()) == '') {
-				if (confirmAlert) {
-					setTimeout(function() {
-						xmActions.confirmAlert(_nextAction);
-					}, 1000);
+			setTimeout(function() {
+				if ($.trim($("#doing").html()) == '') {
+					if (confirmAlert) {
+						setTimeout(function() {
+							xmActions.confirmAlert(_nextAction);
+						}, 1000);
+					} else {
+						setTimeout(_nextAction, 1000);
+					}
 				} else {
-					setTimeout(_nextAction, 1000);
+					setTimeout(function() {
+						xmActions.waitFinish(_nextAction, confirmAlert);
+					}, 5000);
 				}
-			} else {
-				setTimeout(function() {
-					xmActions.waitFinish(_nextAction, confirmAlert);
-				}, 5000);
-			}
+			}, 2000);
 		},
 		moveOutCat: function() {
 			xmActions.debug('xmActions.moveOutCat');
@@ -319,6 +332,43 @@ if (location.href.indexOf("app0.mixi") >= 0 || location.href.indexOf("app.mbga")
 			xmActions.clickAction('#build-button img', function() {
 				xmActions.confirmDialog(_nextAction);
 			});
+		},
+		startTraining: function() {
+			xmActions.debug('xmActions.startTraining');
+			// check resources
+			var resourceTypes = xmModel.resources[xmModel.train.levelType].train;
+			var resources = xmModel.train.level[xmModel.train.startLevel].resources;
+			var hasResources = true;
+			for (var i = 0; i < resourceTypes.length; i++) {
+				if (resources[i] > parseInt($('#element_' + resourceTypes[i]).text())) {
+					hasResources = false;
+				}
+			}
+			if (hasResources) {
+				$('.' + xmModel.train.catClass).simulate('drag-n-drop', {dropTarget: $('img.' + xmModel.resources[xmModel.train.levelType].buildingClass)});
+				setTimeout(function() {
+					xmActions.confirmDialog(function() {
+						var timer = xmModel.train.level[xmModel.train.startLevel].time;
+						console.log('timer: ' + timer);
+						setTimeout(xmActions.waitTraining, timer * 1000);
+					});
+				}, 2000);
+			} else {
+				console.log('not enough resources.');
+			}
+		},
+		waitTraining: function() {
+			xmActions.debug('xmActions.waitTraining');
+			if ($.trim($("#doing").text()).indexOf($('.' + xmModel.train.catClass).prop('alt')) > -1) {
+				setTimeout(xmActions.waitTraining, 10000);
+			} else {
+				xmModel.train.startLevel = xmModel.train.startLevel + 1;
+				if (xmModel.train.startLevel < xmModel.train.endLevel) {
+					xmActions.startTraining();
+				} else {
+					console.log('training finish.');
+				}
+			}
 		}
 	};
 
@@ -327,6 +377,13 @@ if (location.href.indexOf("app0.mixi") >= 0 || location.href.indexOf("app.mbga")
 			case 'startTrial':
 				xmModel.questId = message.questId;
 				xmActions.doQuest();
+				break;
+			case 'startTraining':
+				xmModel.train.catClass = xmModel.train.catPrefix + message.catId;
+				xmModel.train.startLevel = message.startLevel;
+				xmModel.train.endLevel = message.endLevel;
+				xmModel.train.levelType = message.levelType;
+				xmActions.startTraining();
 				break;
 			case 'unitTest':
 				console.log('test.');
--- popup.js
+++ popup.js
@@ -15,4 +15,16 @@ document.addEventListener('DOMContentLoaded', function() {
 		  });
 		});
 	});
+
+	document.getElementById('startTraining').addEventListener('click', function() {
+		var catId = $('#catId').val();
+		var startLevel = parseInt($('#startLevel').val());
+		var endLevel = parseInt($('#endLevel').val());
+		var levelType = $("input[name=levelType]:checked").val();
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		  chrome.tabs.sendMessage(tabs[0].id, {type: "startTraining", catId: catId, startLevel: startLevel, endLevel: endLevel, levelType: levelType}, function(response) {
+			console.log(response.farewell);
+		  });
+		});
+	});
 });
